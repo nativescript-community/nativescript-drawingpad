0a786daeec482de3cc720e3aeae3c78d6267fd83
bradmartin <bradmartin0905@gmail.com>
2016-02-07 23:31:17 -0600

    updated

--- demo/app/main-page.js
+++ demo/app/main-page.js
@@ -1,17 +1,38 @@
 var observable = require("data/observable").Observable;
 var app = require("application");
+var platformModule = require("platform");
 var color = require("color");
+var frame = require("ui/frame");
 
 var data = new observable({});
 
 function pageLoaded(args) {
-    var page = args.object;
+    var page = args.object; 
     page.bindingContext = data;
+    // Change statusbar color on Lollipop
+    if (platformModule.device.sdkVersion >= "21") {
+        var window = app.android.startActivity.getWindow(); 
+        window.setStatusBarColor(new color.Color("#0288D1").android);
+    }
 }
 exports.pageLoaded = pageLoaded;
 
+function padLoaded(args) {
+    console.log(args.object);
+    console.log('native pad: ' + args.object.android);
+}
+exports.padLoaded = padLoaded;
+
 
 function getDrawing(args) {
-    console.log(args.object);
+    var pad = frame.topmost().currentPage.getViewById("drawingPad");
+    var pic = pad.drawing;
+    console.log(pic);
 }
 exports.getDrawing = getDrawing;
+
+function clearDrawing(args) {
+    var pad = frame.topmost().currentPage.getViewById("drawingPad");
+    pad.clear();
+}
+exports.clearDrawing = clearDrawing;
