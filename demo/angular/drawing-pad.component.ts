259df2171638711c685eb92a6e66b36bd5db1b17
leocaseiro <leobok@gmail.com>
2016-06-16 12:03:04 +1000

    feat(Demo): Add Angular app Demo
    Closes #2

---
+++ demo/angular/drawing-pad.component.ts
@@ -0,0 +1,47 @@
+import {Component, ElementRef, ViewChild} from '@angular/core';
+import {registerElement} from "nativescript-angular/element-registry";
+
+registerElement("DrawingPad", () => require("nativescript-drawingpad").DrawingPad);
+
+@Component({
+    selector: 'drawing-pad-example',
+    template: `
+    <ScrollView>
+        <StackLayout>
+            <DrawingPad #DrawingPad 
+            height="400" 
+            id="drawingPad" 
+            penColor="#ff4081" penWidth="3">
+            </DrawingPad>
+
+            <StackLayout orientation="horizontal">
+                <Button text="Get Drawing" (tap)="getMyDrawing()"></Button>
+                <Button text="Clear Drawing" (tap)="clearMyDrawing()"></Button>
+            </StackLayout>
+        </StackLayout>
+    </ScrollView>
+    `
+})
+export class DrawingPadExample {
+
+    @ViewChild("DrawingPad") DrawingPad: ElementRef;
+
+    getMyDrawing(args) {
+        // get reference to the drawing pad
+        let pad = this.DrawingPad.nativeElement;
+
+        // then get the drawing (Bitmap on Android) of the drawingpad
+        let drawingImage;
+        pad.getDrawing().then(function(data) {
+            console.log(data);
+            drawingImage = data;
+        }, function(err) {
+            console.log(err);
+        });
+    }
+
+    clearMyDrawing(args) {
+        var pad = this.DrawingPad.nativeElement;
+        pad.clearDrawing();
+    }
+}
---
+++ demo/angular/main.ts
@@ -0,0 +1,4 @@
+import {nativeScriptBootstrap} from "nativescript-angular/application";
+import {App} from "./App";
+
+nativeScriptBootstrap(App);
